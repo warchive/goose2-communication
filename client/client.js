abdb6c0fefc913d8736d66ba70a8d0e678a4111b
nikolaevra <nikolaevra@gmail.com>
2017-06-07 01:09:11 -0400

    - added comments to some code to make it more clear what is going on

--- client/client.js
+++ client/client.js
@@ -1,4 +1,6 @@
-var socket = require('socket.io-client')('http://192.168.1.142:8002');
+var socket = require('socket.io-client')('http://192.168.1.142:8002'); // ip will probably change all the time
+// on the rpi. Will have to hard code it at some point
+// TODO: give a static ip address to rpi
 
 socket.on('connect', function(){
     console.log("connected");
@@ -7,7 +9,9 @@ socket.on('event', function(data){
     console.log(data);
 });
 
-socket.on('pi', function(data){
+socket.on('pi', function(data){ // so this is our main listener.
+    // when we emit a message on the server side, we can specify what type of message it is
+    // its called pi in this case, but can be anything else
     console.log(data);
 });
 
--- server/server.js
+++ server/server.js
@@ -10,22 +10,26 @@ var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
 var parsers = serialport.parsers;
 
-var port = new SerialPort("/dev/ttyACM0", {
-  	baudrate: 9600,
-  	parser: parsers.readline('\r\n')
+var port = new SerialPort("/dev/ttyACM0", { // if you get an error that this file does not exist,
+    // make sure that you plug in the arduino via a serial port first. That will create the file in rpi
+    baudrate: 9600,
+    parser: parsers.readline('\r\n')
 });
 
 // ===============================Server Setup===========================
 const netPort = 8002;
-server.listen(netPort); //start the webserver on port 8080
-app.use(express.static('public')); //tell the server that ./public/ contains the static webpages
+server.listen(netPort); // start the web server on port 8080
+app.use(express.static('public')); // we don't need this unless we want to serve some static content from rpi
 console.log("listening on port:" + netPort);
 
 // ==============================Sending data======================
 io.sockets.on('connection', function (socket) {
-  	socket.emit('pi', { status: 'opened' });
-	port.on('data', function(data) {
-		socket.emit('pi', {val: data});
-	});  
-	console.log('Socket is open'); // log to console, once serial connection is established
+    socket.emit('pi', { status: 'opened' });
+
+    port.on('data', function(data) { // triggered every time there is data coming from the serial port
+        socket.emit('pi', {val: data}); // this is what actually sensor reading messages through the websocket
+        // by specifying 'pi', i'm obligates to listen to pi events ont he client side
+    });
+
+    console.log('Socket is open'); // log to console, once serial connection is established
 });
