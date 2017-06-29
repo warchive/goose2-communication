f044da2609314783867f19fa478426e900150254
nikolaevra <nikolaevra@gmail.com>
2017-06-28 20:21:46 -0400

    - incremental changes and fixes

--- server/server.js
+++ server/server.js
@@ -6,8 +6,9 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);	//web socket server
 const SimpleNodeLogger = require('simple-node-logger'); // create a custom timestamp format for log statements
-const fileWriter = require('./fileWriter.js');
 const serialport = require('serialport');
+const util = require('util');
+const fs = require('fs');
 
 var dataLog = [];
 dataLog.push({val: "test"});
@@ -51,7 +52,7 @@ io.sockets.on('disconnect', function() {
     log.info('Client count: ' + connectCounter);
     connectCounter--;
     if (connectCounter === 0) {
-        saveData();
+        writer(dataLog);
     }
 });
 
@@ -82,23 +83,30 @@ io.sockets.on('connection', function (socket) {
 rl.on('line', function (input)  {
     if (input === 'save') {
 	console.log("cmd: save");
-        saveData();
+        writer(dataLog);
     } else {
         console.log("Command is not supported");
     }
 });
 
 
-function saveData() {
-    fileWriter.writeFile(JSON.stringify({data: dataLog}), function (result) {
-        if (result) {
-            console.log("Data saved into a file");
-            dataLog = [];
-        } else {
-            console.log(result);
-        }
+function writer (data) {
+    var now = new Date(),
+        d = now.getDate(),
+        m = now.getMonth(),
+        y = now.getFullYear(),
+        h = now.getHours(),
+        min = now.getMinutes(),
+        sec = now.getSeconds(),
+        mill = now.getMilliseconds(),
+        filename = util.format('server/launch-records/%s_%s_%s_%s_%s_%s_%s.json', d, m , y, h, min, sec, mill);
+
+    fs.writeFile(filename, JSON.stringify(data), function (err) {
+        if (err) throw err;
+        console.log('It\'s saved! in same location.');
     });
 }
+
 
 
 
