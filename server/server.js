5a625a2b8d725039bfcfd9de1a93715539b8e78d
Tyler Zhang <TZTylerzhang@gmail.com>
2017-08-27 01:09:35 -0400

    format document

--- server/server.js
+++ server/server.js
@@ -49,32 +49,41 @@ console.log("listening on port:" + CONFIG.PORT);
 io.sockets.on('connection', function (socket) {
     log.info('Client count: ' + ++connectCounter);
 
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function () {
         log.info('Client count: ' + --connectCounter);
         if (connectCounter === 0) {
             H.writer(RAW_FILE, dataLog, fs);
         } else {
-            socket.emit('message', JSON.stringify({time: 0, message: "client count: " + connectCounter}));
+            socket.emit('message', JSON.stringify({
+                time: 0,
+                message: "client count: " + connectCounter
+            }));
         }
     });
 
-    socket.on('control', function(data) {
-	let parsed;
-	try{
-	    parsed = JSON.parse(data);
-        }catch(e){
+    socket.on('control', function (data) {
+        let parsed;
+        try {
+            parsed = JSON.parse(data);
+        } catch (e) {
             console.error("JSON parsing error on socket control");
             return;
         }
         console.log(parsed);
 
-        port.write(data, function(err) {
+        port.write(data, function (err) {
             if (err) {
                 log.error('Failed to send data over serial port: ', err.message, ' data: ', data);
-                socket.emit('message', JSON.stringify({time: 0, message: "cmd FAILED: " + data.toString()}));
+                socket.emit('message', JSON.stringify({
+                    time: 0,
+                    message: "cmd FAILED: " + data.toString()
+                }));
             } else {
                 log.info('Successful cmd: ', data);
-                socket.emit('message', JSON.stringify({time: 0, message: "cmd SUCCESSFUL: " + data.toString()}));
+                socket.emit('message', JSON.stringify({
+                    time: 0,
+                    message: "cmd SUCCESSFUL: " + data.toString()
+                }));
                 console.log(data);
             }
         });
@@ -85,42 +94,57 @@ io.sockets.on('connection', function (socket) {
             }
 
             timer = setInterval(function () {
-                port.write(JSON.stringify({cmd: "check", val:[1]}));
+                port.write(JSON.stringify({
+                    cmd: "check",
+                    val: [1]
+                }));
             }, CONFIG.CHECK_DELAY);
         }
     });
 
 
-    socket.on('trigger_save', function() {
+    socket.on('trigger_save', function () {
         H.writer(RAW_FILE, dataLog, fs);
         log.info("save triggered from client");
-        socket.emit('message', JSON.stringify({time: 0, message: "saved raw out file"}));
+        socket.emit('message', JSON.stringify({
+            time: 0,
+            message: "saved raw out file"
+        }));
     });
 
-    socket.on('save', function(data) {
+    socket.on('save', function (data) {
         if (data === 1) {
             RAW_OUT = true;
             console.log("started writing data to a file");
-            socket.emit('message', JSON.stringify({time: 0, message: "started writing data to a file"}));
+            socket.emit('message', JSON.stringify({
+                time: 0,
+                message: "started writing data to a file"
+            }));
         } else {
             RAW_OUT = false;
             console.log("stopped writing data to a file");
-            socket.emit('message', JSON.stringify({time: 0, message: "stopped writing data to a file"}));
+            socket.emit('message', JSON.stringify({
+                time: 0,
+                message: "stopped writing data to a file"
+            }));
         }
     });
 
-    port.on('data', function(data) {
+    port.on('data', function (data) {
         // console.log("with <3 from arduino: " + data);
 
-	let obj;
-	try{
-	    obj = JSON.parse(data);
-        }catch(e){
+        let obj;
+        try {
+            obj = JSON.parse(data);
+        } catch (e) {
             console.error("JSON parsing error on port data");
             return;
         }
         try {
-            if (obj.hasOwnProperty('received')){
+            if (obj.hasOwnProperty('type') && obj.type === 'state') {
+
+            }
+            if (obj.hasOwnProperty('received')) {
                 obj.received = JSON.parse(obj.received);
                 socket.emit('command_received', JSON.stringify(obj));
             } else if (obj.hasOwnProperty('sensor')) {
@@ -128,19 +152,21 @@ io.sockets.on('connection', function (socket) {
                 if (obj.sensor === 'gyro') {
                     py_gyro.push(obj);
                     if (py_gyro.length > 60) {
-                    //    pyshell.send(JSON.stringify({to_parse: py_gyro}));
+                        //    pyshell.send(JSON.stringify({to_parse: py_gyro}));
                         py_gyro = [];
                     }
-                } if (obj.sensor === 'accel') {
+                }
+                if (obj.sensor === 'accel') {
                     py_accel.push(obj);
                     if (py_accel.length > 60) {
-                    //    pyshell.send(JSON.stringify({to_parse: py_accel}));
+                        //    pyshell.send(JSON.stringify({to_parse: py_accel}));
                         py_accel = [];
                     }
-                } if (obj.sensor === 'mag') {
+                }
+                if (obj.sensor === 'mag') {
                     py_mag.push(obj);
                     if (py_mag.length > 60) {
-                    //    pyshell.send(JSON.stringify({to_parse: py_mag}));
+                        //    pyshell.send(JSON.stringify({to_parse: py_mag}));
                         py_mag = [];
                     }
                 } else {
@@ -156,8 +182,11 @@ io.sockets.on('connection', function (socket) {
         if (RAW_OUT) {
             dataLog.push(data);
             if (dataLog.length === CONFIG.RAW_OUT_BUFF) {
-                console.log("saved "+CONFIG.RAW_OUT_BUFF+" lines into a file");
-                socket.emit('message', JSON.stringify({time: 0, message: "saved "+CONFIG.RAW_OUT_BUFF+" lines to raw out"}));
+                console.log("saved " + CONFIG.RAW_OUT_BUFF + " lines into a file");
+                socket.emit('message', JSON.stringify({
+                    time: 0,
+                    message: "saved " + CONFIG.RAW_OUT_BUFF + " lines to raw out"
+                }));
                 H.writer(RAW_FILE, dataLog, fs);
                 dataLog = [];
             }
@@ -165,7 +194,7 @@ io.sockets.on('connection', function (socket) {
     });
 
     //pyshell.on('message', function (out) {
-        // received a message sent from the Python script (a simple "print" statement)
+    // received a message sent from the Python script (a simple "print" statement)
     //    let parsed = JSON.parse(out);
     //    for (let i = 0; i < out.parsed.length; i++) {
     //        socket.emit('sensor', JSON.stringify(parsed[i]));
