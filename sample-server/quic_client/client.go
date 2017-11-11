f378c613c2dec993c6ed0f15f6e1a2b51b26648f
Edwin Zhang <edwinzhang64@gmail.com>
2017-11-11 14:18:47 -0500

    Streaming to multiple ports

--- sample-server/quic_client/client.go
+++ sample-server/quic_client/client.go
@@ -4,62 +4,67 @@ import (
 	"crypto/tls"
 	"fmt"
 	"strconv"
-	"time"
+	"sync"
 	// "time"
 	"github.com/lucas-clemente/quic-go"
-	// import kcp package
 )
 
 // Simple error verification
-/*func CheckError(err error) 	{
+func CheckError(err error) 	{
 	if err != nil {
 		fmt.Println("Error: ", err)
-
-	}
-}*/
-
-const addr = "10.173.212.248:10000"
-
-func main() {
-	// Server address to send packets
-	config := quic.Config{RequestConnectionIDOmission: false}
-	session, err := quic.DialAddr(addr, &tls.Config{InsecureSkipVerify: true}, &config)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for j := 0; j < 50; j++ {
-		stream, err := session.OpenStream()
-		if err != nil {
-			fmt.Println(err)
-		}
-		//CheckError(err)
-
-		buf2 := make([]byte, 1024) //allocating memory for each integer
-		go func(j int) {
-			for i := 0; i < 1000; i++ {
-				sendPacket(j*1000+i, &stream, buf2)
-			}
-		}(j)
-	}
-	for {
-		time.Sleep(time.Millisecond * 1000)
 	}
 }
 
-func sendPacket(i int, stream *quic.Stream, buf2 []byte) {
-	msg := `{"id": "12313", type": "hello", "data": { "n": ` + strconv.Itoa(i) + `}}`
+const addr1 = "10.173.212.248:10000"
+const addr2 = "10.173.212.248:12345"
+
+func main() {
+	// Open sessions to send packets
+	config := quic.Config{RequestConnectionIDOmission: false}
+
+	var sessions [2]quic.Session
+
+	session1, err := quic.DialAddr(addr1, &tls.Config{InsecureSkipVerify: true}, &config)
+	CheckError(err)
+	session2, err := quic.DialAddr(addr2, &tls.Config{InsecureSkipVerify: true}, &config)
+	CheckError(err)
+
+	sessions[0] = session1
+	sessions[1] = session2
+
+	// Open multiple streams with waitgroup so main doesn't close before the streams finish sending
+	var wg sync.WaitGroup
+	for i := 0; i < 2; i++ {
+			wg.Add(1)
+			defer wg.Done()
+			go OpenStream(i, &sessions[i]);
+	}
+	wg.Wait()
+}
+
+// Open a new stream to send data over
+func OpenStream(j int, session *quic.Session) {
+	buf2 := make([]byte, 1024) //allocating memory
+	stream, err := (*session).OpenStream()
+	CheckError(err)
+	for i := 0; i < 1000; i++ {
+		SendPacket(j*1000+i, &stream, buf2)
+		fmt.Println("hello " + strconv.Itoa(i))
+	}
+}
+
+func SendPacket(i int, stream *quic.Stream, buf2 []byte) {
+	msg := `{"id": "1234567890", "type": "hello", "data": { "n": ` + strconv.Itoa(i) + `}}`
 
 	buf := []byte(msg)
 	_, err := (*stream).Write(buf) // Write a message to the server
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
+	CheckError(err)
 
 	n, err := (*stream).Read(buf2) // Read a message from the server
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
-		_ = n
-		//mt.Printf("%s\n", buf2[0:n])
+		fmt.Printf("%s\n", buf2[0:n])
 	}
 }
